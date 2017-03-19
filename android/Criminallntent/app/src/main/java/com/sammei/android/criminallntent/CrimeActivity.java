e3780403b6096940cf36c7b5a15b66ff1520aa67
sam <shaoming.mei@gmail.com>
2017-03-19 22:47:56 +0800

    新增：Android编程权威指南（第2版）第七章例子

---
+++ android/Criminallntent/app/src/main/java/com/sammei/android/criminallntent/CrimeActivity.java
@@ -0,0 +1,23 @@
+package com.sammei.android.criminallntent;
+
+import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
