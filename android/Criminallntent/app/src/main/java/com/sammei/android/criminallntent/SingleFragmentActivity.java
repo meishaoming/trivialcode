a52f4ffbcd2d6ae0d0812d91f7e7239ebe67f659
sam <shaoming.mei@gmail.com>
2017-03-23 17:23:15 +0800

    更新：Android编程权威指南（第2版）第十三章例子

--- android/Criminallntent/app/src/main/java/com/sammei/android/criminallntent/SingleFragmentActivity.java
+++ android/Criminallntent/app/src/main/java/com/sammei/android/criminallntent/SingleFragmentActivity.java
@@ -1,15 +1,11 @@
 package com.sammei.android.criminallntent;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
-import java.util.UUID;
-
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
